47a357b6b1021b3d84b6d128af9eaca6c348b384
ananthrkn <ananth@iastate.edu>
2017-01-02 12:19:00 +0530

    Disjoint Set

---
+++ DisjointSet/src/DisjointSet.java
@@ -0,0 +1,49 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ * Created by ananthrkn on 1/2/17.
+ */
+public class DisjointSet {
+
+    HashMap<Integer, Integer> map;
+
+    public DisjointSet(){
+        map = new HashMap<>();
+    }
+
+    public boolean operation(int[] a, int[] b){
+        for(int i = 0; i < a.length; i++){
+            map.put(a[i], i);
+        }
+
+        for(int i = 0; i < b.length; i++){
+            if(map.containsKey(b[i])){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter size of first array");
+        int size1 = input.nextInt();
+        int a[] = new int[size1];
+        for(int i = 0; i < size1; i++){
+            a[i] = input.nextInt();
+        }
+
+        System.out.println("Enter size of second array");
+        int size2 = input.nextInt();
+        int b[] = new int[size2];
+        for(int i = 0; i < size2; i++){
+            b[i] = input.nextInt();
+        }
+
+        DisjointSet d = new DisjointSet();
+        boolean bo = d.operation(a, b);
+        System.out.println(bo);
+
+    }
+}
